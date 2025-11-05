d947827d4b6105ab6de92c09209131ca30fb456a
Jonathan Kwak <jkwaktdchristian@gmail.com>
2025-11-04 21:12:26 -0500

    created

---
+++ cps109_a1.py
@@ -0,0 +1,18 @@
+"""
+
+Must have all of these:
+1. Conditions and decision making using if/else statements.
+2. Working with sequences, such as lists, tuples, and strings.
+3. Sequence iteration using for loops, general iteration using wile loops.
+4. Importing namespaces, using their functions, writing your own functions.
+5. Performing file I/O, reading input from a file, writing output to a file.
+
+Problem:
+Pathfinding, A* algorithm
+
+"""
+
+f = open("data.txt", "r")
+content = f.readlines()
+print(content)
+f.close()
